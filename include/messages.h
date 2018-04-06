c0c4cdf9c4c66ec6dff6e29b3f657f38454e81e6
Pierre Gabory <piergabory@gmail.com>
2018-04-06 23:30:26 +0200

    add quotes to messages

--- include/messages.h
+++ include/messages.h
@@ -1,11 +1,13 @@
 #ifndef messages_h
 #define messages_h
 
+// all console messages are here.
+
 #define ERR_SDL_INIT "Impossible d'initialiser la SDL. Fin du programme.\n"
 #define ERR_WINDOW_INIT "Impossible d'ouvrir la fenetre. Fin du programme.\n"
 #define ERR_MALLOC "Out of memory\n"
-#define ERR_FILE_OPEN "Couldn't open file at %s.\n"
-#define ERR_FILE_END "Unexpected end of file %s.\n"
+#define ERR_FILE_OPEN "Couldn't open file at \"%s\".\n"
+#define ERR_FILE_END "Unexpected end of file \"%s\".\n"
 #define ERR_FILE_FORMAT "World files must be in P6 ppm image format (%s).\n"
 
 
