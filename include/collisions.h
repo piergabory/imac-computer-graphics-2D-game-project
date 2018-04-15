8325b22c94b7052c66acd21cb2987af9537b3392
Pierre Gabory <piergabory@gmail.com>
2018-04-15 20:20:27 +0200

    setHitbox()

--- include/collisions.h
+++ include/collisions.h
@@ -4,8 +4,22 @@
 #include <math.h>
 
 #include "gamedata.h"
+#include "game.h"
 
 int isMobOnTerrain(Mob m, Level l);
 int isMobOnMob(Mob m1, Mob m2);
+
+// mob sizes relative to a sprite size
+#define ENEMY_W 1
+#define ENEMY_H 1
+
+#define PLAYER_W 1
+#define PLAYER_H 1
+
+#define BONUS_W 1
+#define BONUS_H 1
+
+#define PROJECTILE_W 1
+#define PROJECTILE_H 0.2
 
 #endif /* collisions_h */
--- src/collisions.c
+++ src/collisions.c
@@ -1,5 +1,30 @@
 #include "../include/collisions.h"
 
+static void setHitbox(char mobType, float *w, float *h) {
+    float scale = 1/getLevel().height;
+    switch (mobType) {
+        case ENEMY:
+            *w = scale * ENEMY_W;
+            *h = scale * ENEMY_H;
+            break;
+            
+        case PLAYER:
+            *w = scale * PLAYER_W;
+            *h = scale * PLAYER_H;
+            break;
+            
+        case BONUS:
+            *w = scale * BONUS_W;
+            *h = scale * BONUS_H;
+            break;
+            
+        case PROJECTILE:
+            *w = scale * PROJECTILE_W;
+            *h = scale * PROJECTILE_H;
+            break;
+    }
+}
+
 int isMobOnTerrain(Mob m, Level l){
     int col = (int)(m.px * l.width) % l.width ;
     int row = (int)(m.py * l.height) % (l.height-1);
@@ -18,6 +43,10 @@ int isMobOnTerrain(Mob m, Level l){
 }
 
 int isMobOnMob(Mob m1, Mob m2){
+    float m1w, m1h, m2w, m2h;
+    setHitbox(m1.type, &m1w, &m1h);
+    setHitbox(m2.type, &m2w, &m2h);
+    
     int deltaX = m1.px - m2.px;
     int deltaY = m1.py - m2.py;
     return !deltaX && !deltaY ;
