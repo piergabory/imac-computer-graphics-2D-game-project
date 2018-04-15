38f9a9f9b3ea2a688b23d2b368a1938cbc129f31
Pierre Gabory <piergabory@gmail.com>
2018-04-15 11:14:36 +0200

    collision file

---
+++ include/collisions.h
@@ -0,0 +1,9 @@
+#ifndef collisions_h
+#define collisions_h
+
+#include "gamedata.h"
+
+int isMobiOnTerrain(Mob m, Level l);
+int isMobiOnMob(Mob m1, Mob m2);
+
+#endif /* collisions_h */
---
+++ src/collisions.c
@@ -0,0 +1,11 @@
+#include "../include/collisions.h"
+
+int isMobiOnTerrain(Mob m, Level l){
+    
+    
+    return 0;
+}
+
+int isMobiOnMob(Mob m1, Mob m2){
+    return 0;
+}
