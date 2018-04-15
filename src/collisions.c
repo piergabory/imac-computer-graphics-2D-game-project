16533b8322429b1125984cf40bc9baec5196a935
Pierre Gabory <piergabory@gmail.com>
2018-04-15 19:25:48 +0200

    élargissement de la zone d'impact sur le terrain

--- src/collisions.c
+++ src/collisions.c
@@ -1,12 +1,19 @@
 #include "../include/collisions.h"
 
 int isMobOnTerrain(Mob m, Level l){
-    int mapColumn = m.px * l.width;
-    int mapRow = m.py * l.height;
-    if (l.map[mapRow % l.height][mapColumn % l.width] != 0){
-        l.map[mapRow % l.height][mapColumn % l.width] = 0;
+    int col = (int)(m.px * l.width) % l.width ;
+    int row = (int)(m.py * l.height) % (l.height-1);
+    
+    if (l.map[row][col] != 0){
+        l.map[row][col] = 0;
         return 1;
     }
+    
+    else if (l.map[row+1][col] != 0) {
+        l.map[row+1][col] = 0;
+        return 1;
+    }
+    
     return 0;
 }
 
