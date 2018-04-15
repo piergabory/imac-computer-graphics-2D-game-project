5ebd9bcd73ed08f4aee54ff653051a9044ad7b9b
Pierre Gabory <piergabory@gmail.com>
2018-04-15 12:23:21 +0200

    collisions

--- include/collisions.h
+++ include/collisions.h
@@ -1,9 +1,11 @@
 #ifndef collisions_h
 #define collisions_h
 
+#include <math.h>
+
 #include "gamedata.h"
 
-int isMobiOnTerrain(Mob m, Level l);
-int isMobiOnMob(Mob m1, Mob m2);
+int isMobOnTerrain(Mob m, Level l);
+int isMobOnMob(Mob m1, Mob m2);
 
 #endif /* collisions_h */
--- src/collisions.c
+++ src/collisions.c
@@ -1,11 +1,17 @@
 #include "../include/collisions.h"
 
-int isMobiOnTerrain(Mob m, Level l){
-    
-    
+int isMobOnTerrain(Mob m, Level l){
+    int mapColumn = m.px * l.width - l.progress;
+    int mapRow = m.py * l.height;
+    if (l.map[mapColumn % l.height][mapRow % l.width] != 0){
+        l.map[mapColumn % l.height][mapRow % l.width] = 0;
+        return 1;
+    }
     return 0;
 }
 
-int isMobiOnMob(Mob m1, Mob m2){
-    return 0;
+int isMobOnMob(Mob m1, Mob m2){
+    int deltaX = m1.px - m2.px;
+    int deltaY = m1.py - m2.py;
+    return !deltaX && !deltaY ;
 }
